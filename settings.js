23db0f3271a273a8a40f03e626a895f23d7d4436
Haan90 <159756440+Haan90@users.noreply.github.com>
2024-02-25 17:03:08 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,32 @@
+/*
+• Base Ori GhostXmods X WannOFFC
+Penting ‼️
+
+Hapus Bagian/Teks Ini? Masuk Neraka Paling Bawah
+
+Script Ini Murni Bikinan Sendiri, Saya Hanya Sekedar Kroco Penghuni Inti Bumi.
+
+Thanks To :                                
+- Allah Swt 
+- Nabi Muhammad Saw         
+- My Parents       
+- Ghost [ Develover Sc ]  
+- WannOFFC [ Support ]
+- Pengguna Bot Yang Selalu Support
+
+• Recode By ( Nama Mu )
+*/
+
+const settings = {
+  token: '6900757287:AAFfS1efIHIWAn51XDUFdsunymjxNFmuMKM', // Token Bot
+  adminId: '6670680707',
+  urladmin: 'https://t.me/Denzxxoffc',
+  domain: 'https://haxprivate.mypanell.biz.id', // Isi dengan domain yang digunakan
+  plta: 'ptla_e8SFVbghtSsTc4xX8lwYXCBsCCS1HNjObs29nvZh5S8', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_kQ7sv0zH1H77Wv0dYd80qTnHPghE1wwilH2Wz7ZFOmD', // Isi dengan nilai pltc yang sesuai
+  pp: 'https://telegra.ph/file/8ef2d072f8c68f120d18d.jpg',
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
